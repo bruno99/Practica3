e4a5483e5e9c0464ba89d0975aaee36afe0b4798
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2020-11-27 12:30:20 +0100

    Create api.ts

---
+++ api.ts
@@ -0,0 +1,30 @@
+import { Application, Router, helpers } from 'https://deno.land/x/oak@v6.3.1/mod.ts';
+import { v4 } from 'https://deno.land/std@0.74.0/uuid/mod.ts';
+import { MongoClient} from "https://deno.land/x/mongo@v0.12.1/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+import { Database } from "https://deno.land/x/mongo@v0.12.1/ts/database.ts";
+
+ 
+const port = 8000;
+const app = new Application();
+
+const DB_URL = Deno.env.get("DB_URL");
+const DB_NAME = Deno.env.get("DB_NAME");
+
+  if (!DB_URL || !DB_NAME) {
+    throw Error("Please define DB_URL and DB_NAME on .env file");
+  }
+
+const client = new MongoClient();
+client.connectWithUri(DB_URL);
+const db = client.database(DB_NAME);
+
+app.use(async (ctx, next) => {
+  ctx.state.db = db;
+  await next();
+});
+
+app.addEventListener('listen', () => {
+    console.log(`Listening on localhost:${port}`);
+  });
+await app.listen({ port });
